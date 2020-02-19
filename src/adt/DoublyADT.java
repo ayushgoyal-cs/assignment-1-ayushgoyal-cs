b0038ce0a5a844d3f9b53d6b6df1dedd55019372
Gopal <gopal@benddit.com>
2020-02-19 20:08:04 +0530

    creating all methods in interface DoublyADT

--- src/adt/DoublyADT.java
+++ src/adt/DoublyADT.java
@@ -1,5 +1,28 @@
 package adt;
 
 public interface DoublyADT<E> {
+    /**
+     This method will add the item in the list.
+     */
+    void add(E item);
 
+    /**
+     This method will remove an item from the linked list.
+     */
+    E remove();
+
+    /**
+     This method will search an item in the list.
+     */
+    int search(E item);
+
+    /**
+     This method will sort the list based on their nodes' data in ascending order.
+     */
+    void sort();
+
+    /**
+    This method will print all the element in the list.
+    */
+    void print();
 }
